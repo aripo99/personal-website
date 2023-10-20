3d63ae3f074721f4f4af274d1f18e689bc708b74
aripo99 <a_polakof@umail.ucsb.edu>
2023-10-20 16:08:00 -0700

    Decrease padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import TopNavBar from "@/components/ui/TopNavBar"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
         <TopNavBar />
       </div>
