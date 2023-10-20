b16fa7cd5bd6ea7b78fb221d3da55ef19b39fb51
aripo99 <a_polakof@umail.ucsb.edu>
2023-10-20 16:19:01 -0700

    Add about me section with random description

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import TopNavBar from "@/components/ui/TopNavBar"
+import TopNavBar from "@/components/TopNavBar"
+import AboutMe from "@/components/AboutMe"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center flex-col justify-center font-mono text-sm lg:flex">
         <TopNavBar />
+        <AboutMe />
       </div>
     </main>
   )
---
+++ components/AboutMe.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+
+const description = "I'm a software engineer based in the Bay Area. I enjoy building things that live on the internet. I develop exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends."
+
+const AboutMe: React.FC = () => {
+    return (
+        <div className="flex justify-between items-center py-8">
+            <Image
+                src={"/images/avatar.jpg"}
+                alt="Your Image Description"
+                className="w-1/4 rounded-full"
+                width={200}
+                height={200}
+            />
+            <div className="w-2/3 ml-8">
+                <p className="font-mono text-lg">{description}</p>
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
