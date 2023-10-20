83731c1f23b4ee5d03a7d8f2105e26fba0727c05
aripo99 <a_polakof@umail.ucsb.edu>
2023-10-20 16:22:15 -0700

    Create about me title

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -5,18 +5,21 @@ const description = "I'm a software engineer based in the Bay Area. I enjoy buil
 
 const AboutMe: React.FC = () => {
     return (
-        <div className="flex justify-between items-center py-8">
-            <Image
-                src={"/images/avatar.jpg"}
-                alt="Your Image Description"
-                className="w-1/4 rounded-full"
-                width={200}
-                height={200}
-            />
-            <div className="w-2/3 ml-8">
-                <p className="font-mono text-lg">{description}</p>
+        <section className="mt-12 mb-12">
+            <h1 className="text-4xl font-semibold tracking-tighter leading-tight mb-6 text-gray-600">About Me</h1>
+            <div className="flex justify-between items-center py-8">
+                <Image
+                    src={"/images/avatar.jpg"}
+                    alt="Your Image Description"
+                    className="w-1/4 rounded-full"
+                    width={200}
+                    height={200}
+                />
+                <div className="w-2/3 ml-8">
+                    <p className="font-mono text-lg">{description}</p>
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
 
