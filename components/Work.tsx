9685fca01e009ccdad207b904aa988027f7c077e
aripo99 <a_polakof@umail.ucsb.edu>
2023-10-23 13:29:03 -0700

    Update work section with techstack chips

--- components/Work.tsx
+++ components/Work.tsx
@@ -13,10 +13,11 @@ function Work() {
             title: "Software Engineer",
             company: "Appfolio",
             projects: [
-                "RealmX: Currently working in RealmX, an assistant-like chatbot for property managers to get quick answers to their data questions, \"What tenants have not paid rent this month\", , product questions \"How do I charge tenants\", and more. I worked on the backend, frontend, and the ML services.",
-                "Invoice Processing: Worked on a machine learning pipeline that automatically extracts data from invoices and receipts. It processes ~13M invoices each year. ",
-                "Bank Feed: A bank reconciliation tool that automatically matches transactions from a bank statement to transactions in Appfolio. I workend on the end to end machine learning pipeline, and it currently matches ~20k transactions each month.",
+                "RealmX: A assistant-like chatbot for property managers to get quick answers to their data questions, \"What tenants have not paid rent this month\", , product questions \"How do I charge tenants\", and more. I worked on the backend, frontend, and the ML services.",
+                "Invoice Processing: Machine learning pipeline that automatically extracts data from invoices and receipts. processing ~13M invoices each year. ",
+                "Bank Feed: Bank reconciliation tool that automatically matches transactions from a bank statement to transactions in Appfolio, matching ~20k transactions each month.",
             ],
+            techStack: ["Rails", "React", "Python", "Rails", "Typescript", "AWS"],
             duration: "2021 - present"
         },
         {
@@ -29,6 +30,7 @@ function Work() {
                 "CS 5B: Data Science 2",
                 "CS 64: Computer Organization and Logic Design",
             ],
+            techStack: ["C++", "Python", "Pandas", "NumPy"],
             duration: "2020 - 2022"
         }
     ];
@@ -50,7 +52,15 @@ function Work() {
                             ))}
                         </CardContent>
                         <CardFooter className="text-gray-300">
-                            {exp.duration}
+                            <div className="flex flex-col">
+                                <div className="flex mb-2">
+                                    {exp.techStack.map((tech, idx) => (
+                                        <span key={idx} className="bg-gray-800 text-gray-300 rounded p-1 text-xs mr-2">{tech}</span>
+                                    ))}
+                                </div>
+                                <div>{exp.duration}</div>
+                            </div>
+
                         </CardFooter>
                     </Card>
                 ))}
