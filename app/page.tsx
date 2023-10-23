06b95690f1f8cc7ad7da5b3856cf3683c3cb0aef
aripo99 <a_polakof@umail.ucsb.edu>
2023-10-23 07:41:43 -0700

    Align items on start

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Books from "@/components/Books"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
-      <div className="z-10 max-w-5xl w-full items-center flex-col justify-center font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-start flex-col justify-center font-mono text-sm lg:flex">
         <TopNavBar />
         <AboutMe />
         <Work />
