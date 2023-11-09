86eaa232e47f7bb6c4f6a445395a99d82a3734bf
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-08 23:19:26 -0800

    Add project github links

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,26 +6,28 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
+import { FaGithub } from 'react-icons/fa';
 
 function Projects() {
     const projectList = [
         {
             name: 'Kognis',
             description: 'An AI-powered education platform that helps students learn more effectively.',
-            // thumbnail: '/path_to_thumbnail_image1.jpg',
             techStack: ['Next.js', 'Flask', 'MongoDB', 'Pinecone'],
             demo: 'https://www.youtube.com/watch?v=CPscvvBMwh0'
         },
         {
             name: 'Aitr',
             description: 'A platform to learn React using AI, adapting exercises and content on the fly.',
-            // thumbnail: '/path_to_thumbnail_image2.jpg',
-            techStack: ['Next.js', 'Supabase', 'Tailwind', 'shadcn/ui'],
+            techStack: ['Next.js', 'Supabase', 'Tailwind'],
+            github: 'https://github.com/aripo99/react-learning-ai-app'
         },
         {
             name: 'Meme Generator',
             description: 'A meme generator that uses the gpt vision API to generate memes from uploaded images.',
-            techStack: ['Next.js', 'Tailwind', 'shadcn/ui']
+            techStack: ['Next.js', 'Tailwind', 'shadcn/ui'],
+            demo: 'https://www.youtube.com/watch?v=BW8J4QgWGJg',
+            github: 'https://github.com/aripo99/meme-generator'
         }
     ];
 
@@ -49,8 +51,19 @@ function Projects() {
                             </div>
                         </CardContent>
                         <CardFooter>
-                            <div className="flex space-x-4">
-                                {project.demo ? <a href={project.demo} target="_blank" rel="noopener noreferrer">Live Demo</a> : null}
+                            <div className="flex justify-between items-center w-full">
+                                {project.demo ? (
+                                    <a href={project.demo} target="_blank" rel="noopener noreferrer" className="text-left">Live Demo</a>
+                                ) : (
+                                    <p className="text-left text-gray-500">Coming soon</p>
+                                )}
+                                <div>
+                                    {project.github && (
+                                        <a href={project.github} target="_blank" rel="noopener noreferrer" className="flex items-center justify-end">
+                                            <FaGithub />
+                                        </a>
+                                    )}
+                                </div>
                             </div>
                         </CardFooter>
                     </Card>
