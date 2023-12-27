e5f900d7dcbe18a62e54d259486e4def18a62925
aripo99 <a_polakof@umail.ucsb.edu>
2023-12-27 14:23:24 -0800

    Use carousel in values

--- components/Values.tsx
+++ components/Values.tsx
@@ -1,14 +1,23 @@
-import Value from "@/components/Value";
+"use client";
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Card, CardContent } from "@/components/ui/card"
 
 const values = [
     {
         title: "Always go the extra step",
     },
     {
-        title: "Doing the right thing",
+        title: "Do the right thing",
     },
     {
-        title: "Continuos improvement",
+        title: "Continuously improve",
     },
     {
         title: "Seek the truth",
@@ -19,21 +28,36 @@ const values = [
     {
         title: "Think slow act fast"
     },
-    {
-        title: "Give a shit"
-    }
+    // {
+    //     title: "Give a shit"
+    // },
+    // {
+    //     title: "Appreciate the little things"
+    // },
+    // {
+    //     title: "Ask questions"
+    // },
 ]
 
 export default function Values() {
     return (
         <section className="my-12" id="projects">
             <h2 className="text-3xl font-bold mb-10 text-gray-200 underline">Values</h2>
-            <div className="flex flex-col space-y-4">
-                {values.map((value, index) => (
-                    <Value key={index} title={value.title} />
-                )
-                )}
-            </div>
-        </section>
+            <Carousel>
+                <CarouselContent className="-ml-1">
+                    {values.map((value, index) => (
+                        <CarouselItem key={index} className="pl-1">
+                            <Card>
+                                <CardContent className="flex items-center justify-center p-6">
+                                    <span className="text-2xl font-semibold">{value.title}</span>
+                                </CardContent>
+                            </Card>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+        </section >
     )
 }
