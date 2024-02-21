e66a465277e9163f80fd9f5c70ff51344ed54722
aripo99 <a_polakof@umail.ucsb.edu>
2024-02-21 10:15:21 -0800

    Add skeleton for reading a blog

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,14 +1,27 @@
 'use client'
 
 import { usePathname } from "next/navigation"
+import Markdown from 'react-markdown'
+import getBlog from "@/lib/actions/get-blog"
+import { useState, useEffect } from "react"
 
 export default function Blog() {
+    const [markdown, setMarkdown] = useState('')
     const pathname = usePathname()
     const blogId = pathname.split('/')[2]
+
+    useEffect(() => {
+        async function fetchBlog() {
+            const response = await getBlog(blogId)
+            setMarkdown(response.markdown)
+        }
+        fetchBlog()
+    }, [blogId])
 
     return (
         <div>
             <h1>Blog {blogId} </h1>
+            <Markdown>{markdown}</Markdown>
         </div>
     )
 }
---
+++ blog/on-moving-to-the-us.md
@@ -0,0 +1,3 @@
+### On Moving to the US
+
+Coming soon... will be leaving everything ready so that I can write once I'm inspired :)
