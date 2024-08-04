b4ce3258e306b20dd5567fad357f97120a9442ea
aripo99 <a_polakof@umail.ucsb.edu>
2024-08-04 11:00:10 -0700

    Fix params type

--- app/blogs/[id]/page.tsx
+++ app/blogs/[id]/page.tsx
@@ -1,9 +1,8 @@
 import Markdown from 'react-markdown'
 import getBlog from "@/lib/actions/get-blog"
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher"
 import { Card, CardContent } from "@/components/ui/card"
 
-export default async function Blog({ params }: Params) {
+export default async function Blog({ params }: { params: { id: string } }) {
     const blogId = params.id
     const resp = await getBlog(blogId)
     const { markdown } = resp
