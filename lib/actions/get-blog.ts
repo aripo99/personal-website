468abbb73bded024fb8fea23142618c4758fbac4
aripo99 <a_polakof@umail.ucsb.edu>
2024-08-04 11:12:02 -0700

    Fix blog file reading

--- lib/actions/get-blog.ts
+++ lib/actions/get-blog.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import fs from 'fs';
+import path from 'path';
 
 export default async function getBlog(id: string) {
-    const path = `blog/${id}.md`;
-    const markdown = fs.readFileSync(path, 'utf-8');
+    const filePath = path.join(process.cwd(), `blog/${id}.md`);
+    const markdown = fs.readFileSync(filePath, 'utf-8');
     return { markdown };
 }
