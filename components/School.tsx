e4ec0bccaac68985284e7e9bb049c74bb9a3825f
aripo99 <a_polakof@umail.ucsb.edu>
2024-08-11 19:53:45 -0700

    Update school

--- components/School.tsx
+++ components/School.tsx
@@ -9,7 +9,7 @@ const EducationTimeline: React.FC = () => {
                 <ol>
                     <li className="mb-10 ml-4">
                         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Current</time>
+                        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Latest</time>
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Masters at Georgia Tech, Machine Learning Specialization</h3>
                         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Notable classes: Deep Learning, Human Computer Interaction, Software Architecture and Design, Machine Learning for Trading, Computer Networks.</p>
                     </li>
