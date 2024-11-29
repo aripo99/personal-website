99f2e47dd2590c205902d4e68af8480b52dbec1f
aripo99 <a_polakof@umail.ucsb.edu>
2024-11-28 17:58:04 -0800

    Add living blog

---
+++ blog/living.md
@@ -0,0 +1,9 @@
+### living
+
+The other day I was listening to a podcast where they mentioned how, as people age, they start to become more and more unhappy with inaction versus action. I have no idea if the research behind that is accurate or not, but I can say that’s definitely true for me. I’d much rather try something and be unhappy with the result than sit around later wondering what if.
+
+There’s something amazing about living life to the fullest—just trying stuff and learning as you go. And I get it, there’s always an excuse not to do the thing you were thinking about. If you think about it long enough, you’ll always find an argument that makes sense. But sometimes, it’s as simple as shifting your mindset and deciding, I’ll try it no matter what. I’ll just get it done.
+
+It’s crazy how deep that is for me. Some of my best memories come from the most random moments—things I had a million excuses not to do but did anyway.
+
+At the same time, I hate the feeling of inaction, of sitting there later wondering what if. That might’ve been the only chance, and now it’s gone forever. You’ll never know what could’ve happened. And for me, I always want to know. So I’ll find out.
--- components/Blog.tsx
+++ components/Blog.tsx
@@ -1,68 +1,72 @@
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
 
 export const pieces = [
-    {
-        title: "just send it",
-        description: "if there’s one thing I want to take from this year...",
-        link: "blog/just-send-it",
-    },
-    {
-        title: "uruguay",
-        description: "aunque vivi la mayoria de mi vida en uruguay...",
-        link: "blog/uruguay",
-    },
-    {
-        title: "reflecting on my time @ appfolio",
-        description: "my story with appfolio goes way back...",
-        link: "blog/reflecting-on-my-time-at-appfolio",
-    },
-    {
-        title: "moving to california",
-        description: "at age 18, i moved to california for college...",
-        link: "blog/moving-to-california",
-    },
-    {
-        title: "friends",
-        description: "most of my life i had friends. growing up in uruguay...",
-        link: "blog/friends",
-    },
-    {
-        title: "happiness",
-        description: "every once in a while i think about what makes me happy...",
-        link: "blog/happiness",
-    }
+  {
+    title: "living",
+    description: "the other day i was listening to a podcast...",
+    link: "blog/living",
+  },
+  {
+    title: "just send it",
+    description: "if there’s one thing I want to take from this year...",
+    link: "blog/just-send-it",
+  },
+  {
+    title: "uruguay",
+    description: "aunque vivi la mayoria de mi vida en uruguay...",
+    link: "blog/uruguay",
+  },
+  {
+    title: "reflecting on my time @ appfolio",
+    description: "my story with appfolio goes way back...",
+    link: "blog/reflecting-on-my-time-at-appfolio",
+  },
+  {
+    title: "moving to california",
+    description: "at age 18, i moved to california for college...",
+    link: "blog/moving-to-california",
+  },
+  {
+    title: "friends",
+    description: "most of my life i had friends. growing up in uruguay...",
+    link: "blog/friends",
+  },
+  {
+    title: "happiness",
+    description: "every once in a while i think about what makes me happy...",
+    link: "blog/happiness",
+  },
 ];
 
 export default function Blog() {
-    return (
-        <section className="my-12" id="blog">
-            <h2 className="text-3xl font-bold mb-10 text-gray-200 underline">Blog</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-                {
-                    pieces.map((blog, index) => (
-                        <Link href={blog.link} key={index}>
-                            <Card>
-                                <CardHeader>
-                                    <CardTitle>{blog.title}</CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <CardDescription>{blog.description}</CardDescription>
-                                </CardContent>
-                                <CardFooter>
-                                <span className="text-blue-500 hover:underline">Read more</span>                                </CardFooter>
-                            </Card>
-                        </Link>
-                    ))
-                }
-            </div>
-        </section>
-    )
+  return (
+    <section className="my-12" id="blog">
+      <h2 className="text-3xl font-bold mb-10 text-gray-200 underline">Blog</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {pieces.map((blog, index) => (
+          <Link href={blog.link} key={index}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{blog.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <CardDescription>{blog.description}</CardDescription>
+              </CardContent>
+              <CardFooter>
+                <span className="text-blue-500 hover:underline">Read more</span>{" "}
+              </CardFooter>
+            </Card>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
