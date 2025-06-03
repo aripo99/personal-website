54e5cd5a7418810716611faf34e757b642f7a105
aripo99 <a_polakof@umail.ucsb.edu>
2025-06-02 21:57:27 -0700

    Update order

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between font-mono px-4">
       <div className="z-10 max-w-5xl w-full items-start flex-col justify-center font-mono text-sm lg:flex">
         <AboutMe />
-        <Projects />
         <Work />
+        <Projects />
         <Values />
         <Blog />
         <School />
